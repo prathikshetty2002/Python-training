9ab8b833c41fef429ed245d1c9c5989f41a15590
Prathik Shetty <63772910+prathikshetty2002@users.noreply.github.com>
2022-12-12 06:03:27 +0000

    day 1 morning session

---
+++ Day1/basics-python.py
@@ -0,0 +1,19 @@
+#python memeory management
+a=10000/10
+b=1000
+c=500*2
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+a=10
+b=20
+print('a is',a,'and b is',b)
+print(a,'is a and',b,'is b')
+print('a is',a,b,'is b')
+
+name='prathik'
+age=20
+
+print(f"hello!, I am {name} and {age} years old.")
